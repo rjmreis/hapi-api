f79a26f4d2427281b03fd923a93a37c0cd08a915
Ricardo Reis <reisr@Ricardos-MacBook-Pro.local>
2016-08-31 12:11:38 +0200

    Updated packages and test structure

--- test/api.js
+++ test/api.js
@@ -1,55 +1,63 @@
-// Load modules
+const Glue = require('Glue');
+const manifest = require('../config/manifest.json');
 
 const Code = require('code');
 const Lab = require('lab');
-
-const Glue = require('glue');
-const manifest = require('../config/manifest.json');
-
-// Test shortcuts
-
 const lab = exports.lab = Lab.script();
 
-// Tests
-Glue.compose(manifest, { relativeTo: process.cwd() }, (err, server) => {
-  server.start(() => { });
+const describe = lab.describe;
+const it = lab.it;
+const before = lab.before;
+const after = lab.after;
+const expect = Code.expect;
 
-  lab.describe("Routes", () => {
+describe('API', () => {
+  var server;
 
-    lab.it('Known route should return http status 200', done => {
-      server.inject('/api', response => {
-        Code.expect(response.statusCode).to.equal(200);
-        done();
-      });
-    });
-    
-    lab.it('Restricted route should return http status 401 for anonymous user', done => {
-      server.inject('/api/restricted', response => {
-        Code.expect(response.statusCode).to.equal(401);
-        done();
-      });
-    });
-    
-    lab.it('Restricted route should return http status 200 for authenticated user', done => {
-      var options = {
-        method : 'GET',
-        url : '/api/restricted',
-        headers : {
-          'Authorization' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFudGhvbnkgVmFsaWQgVXNlciIsImlhdCI6MTQyNTQ3MzUzNX0.KA68l60mjiC8EXaC2odnjFwdIDxE__iDu5RwLdN1F2A',
-          'Content-Type' : 'application/json; charset=utf-8'
-        }
-      };
-      server.inject(options, response => {
-        Code.expect(response.statusCode).to.equal(200);
-        done();
-      });
-    });
+  before((done) => {
 
-    lab.it('Unknown route should return http status 404', done => {
-      server.inject('/api/unkownroute', response => {
-        Code.expect(response.statusCode).to.equal(404);
-        done();
-      });
+    Glue.compose(manifest, { relativeTo: process.cwd() }, (err, hapi) => {
+      server = hapi;
+      server.start(() => { });
+    });
+    done();
+
+  });
+
+  it('Known route should return http status 200', done => {
+    server.inject('/api', response => {
+      expect(response.statusCode).to.equal(200);
+      done();
     });
   });
+
+  it('Restricted route should return http status 401 for anonymous user', done => {
+    server.inject('/api/restricted', response => {
+      expect(response.statusCode).to.equal(401);
+      done();
+    });
+  });
+
+  it('Restricted route should return http status 200 for authenticated user', done => {
+    var options = {
+      method: 'GET',
+      url: '/api/restricted',
+      headers: {
+        'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFudGhvbnkgVmFsaWQgVXNlciIsImlhdCI6MTQyNTQ3MzUzNX0.KA68l60mjiC8EXaC2odnjFwdIDxE__iDu5RwLdN1F2A',
+        'Content-Type': 'application/json; charset=utf-8'
+      }
+    };
+    server.inject(options, response => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Unknown route should return http status 404', done => {
+    server.inject('/api/unkownroute', response => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+
 });
